c00d71fafae215ffb65669d84421efcc71070bcd
Mohamed Abdo <mohamed888rady@gmail.com>
2022-07-13 18:14:53 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// let days = document.getElementById("#days");
+// let hours = document.getElementById("hours");
+// let minutes = document.getElementById("minutes");
+// let seconds = document.getElementById("seconds");
+
+// let countDownDate = new Date("Dec 6, 2022 23:59:59").getTime();
+
+// let counter = setInterval(() => {
+// let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+// let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / 1000 / 60 / 60);
+// let minutes = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / 1000 / 60);
+// let seconds = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / 1000);
+
+// // console.log('.days');
+// document.querySelector("#days").innerText = days;
+// }, 1000);
